4cb9fe100ff4844ae8eee04909644d8e3dcc52b4
Henryk Konsek <hekonsek@gmail.com>
2017-11-29 17:03:38 +0100

    Fixed modifiers order.

--- memory/src/main/java/com/github/hekonsek/rxjava/view/document/memory/InMemoryDocumentView.java
+++ memory/src/main/java/com/github/hekonsek/rxjava/view/document/memory/InMemoryDocumentView.java
@@ -19,29 +19,29 @@ public class InMemoryDocumentView implements DocumentView {
 
     private final Map<String, Map<String, Map<String, Object>>> documents = new LinkedHashMap<>();
 
-    synchronized @Override public Completable save(String collection, String key, Map<String, Object> document) {
+    @Override synchronized public Completable save(String collection, String key, Map<String, Object> document) {
         Map<String, Map<String, Object>> collectionData = documents.computeIfAbsent(collection, k -> new LinkedHashMap<>());
         collectionData.put(key, document);
         return complete();
     }
 
-    synchronized @Override public Single<Map<String, Object>> findById(String collection, String key) {
+    @Override synchronized public Single<Map<String, Object>> findById(String collection, String key) {
         Map<String, Map<String, Object>> collectionData = documents.computeIfAbsent(collection, k -> new LinkedHashMap<>());
         return just(collectionData.get(key));
     }
 
-    synchronized @Override public Single<Long> count(String collection) {
+    @Override synchronized public Single<Long> count(String collection) {
         Map<String, Map<String, Object>> collectionData = documents.computeIfAbsent(collection, key -> new LinkedHashMap<>());
         return just((long) collectionData.size());
     }
 
-    synchronized @Override public Observable<DocumentWithKey> findAll(String collection) {
+    @Override synchronized public Observable<DocumentWithKey> findAll(String collection) {
         List<DocumentWithKey> documentsWithIds = documents.computeIfAbsent(collection, key -> new LinkedHashMap<>()).entrySet().stream().
                 map(entry -> new DocumentWithKey(entry.getKey(), entry.getValue())).collect(toList());
         return fromIterable(documentsWithIds);
     }
 
-    synchronized @Override public Completable remove(String collection, String key) {
+    @Override synchronized public Completable remove(String collection, String key) {
         Map<String, Map<String, Object>> collectionData = documents.computeIfAbsent(collection, k -> new LinkedHashMap<>());
         collectionData.remove(key);
         return complete();
