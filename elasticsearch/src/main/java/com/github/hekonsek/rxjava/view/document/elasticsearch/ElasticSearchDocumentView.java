e35cb053e4e22ecb25248640699be095748ff493
Henryk Konsek <hekonsek@gmail.com>
2017-12-01 09:50:29 +0100

    ES client should be closed if connection cannot be established.

--- elasticsearch/src/main/java/com/github/hekonsek/rxjava/view/document/elasticsearch/ElasticSearchDocumentView.java
+++ elasticsearch/src/main/java/com/github/hekonsek/rxjava/view/document/elasticsearch/ElasticSearchDocumentView.java
@@ -1,3 +1,19 @@
+/**
+ * Licensed to the RxJava View under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.github.hekonsek.rxjava.view.document.elasticsearch;
 
 import com.github.hekonsek.rxjava.view.document.DocumentView;
@@ -11,7 +27,6 @@ import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.action.search.SearchResponse;
-import org.elasticsearch.client.Client;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.TransportAddress;
 import org.elasticsearch.index.IndexNotFoundException;
@@ -32,7 +47,9 @@ import static java.util.stream.Collectors.toList;
  */
 public class ElasticSearchDocumentView implements DocumentView {
 
-    private Client client;
+    private PreBuiltTransportClient client;
+
+    // Configuration members
 
     private String clusterName = "default";
 
@@ -45,10 +62,13 @@ public class ElasticSearchDocumentView implements DocumentView {
     public ElasticSearchDocumentView start() {
         try {
             Settings settings = Settings.builder().put("cluster.name", clusterName).build();
-            client = new PreBuiltTransportClient(settings)
-                    .addTransportAddress(new TransportAddress(InetAddress.getByName(host), port));
+            client = new PreBuiltTransportClient(settings);
+            client.addTransportAddress(new TransportAddress(InetAddress.getByName(host), port));
             return this;
         } catch (UnknownHostException e) {
+            if(client != null) {
+                client.close();
+            }
             throw new RuntimeException(e);
         }
     }
