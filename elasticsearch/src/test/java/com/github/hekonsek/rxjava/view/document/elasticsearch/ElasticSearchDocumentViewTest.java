7c1b26e6e4b28b6991f31c4a04ef83a837dd0f26
Henryk Konsek <hekonsek@gmail.com>
2017-12-20 16:26:49 +0100

    Improved test assertions.

--- elasticsearch/src/test/java/com/github/hekonsek/rxjava/view/document/elasticsearch/ElasticSearchDocumentViewTest.java
+++ elasticsearch/src/test/java/com/github/hekonsek/rxjava/view/document/elasticsearch/ElasticSearchDocumentViewTest.java
@@ -5,9 +5,9 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -42,7 +42,7 @@ import java.util.UUID;
 import static java.util.Collections.emptyMap;
 import static java.util.concurrent.TimeUnit.MINUTES;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.fail;
+import static org.assertj.core.api.Assertions.catchThrowable;
 import static org.junit.rules.Timeout.seconds;
 import static pl.allegro.tech.embeddedelasticsearch.PopularProperties.CLUSTER_NAME;
 import static pl.allegro.tech.embeddedelasticsearch.PopularProperties.TRANSPORT_TCP_PORT;
@@ -81,13 +81,8 @@ public class ElasticSearchDocumentViewTest {
 
     @Test
     public void shouldValidateInvalidHost() {
-        try {
-            new ElasticSearchDocumentView().host("someRandomHost").start();
-        } catch (RuntimeException e) {
-            assertThat(e.getCause() instanceof UnknownHostException);
-            return;
-        }
-        fail("Exception expected");
+        Throwable t = catchThrowable(() -> new ElasticSearchDocumentView().host("someRandomHost").start());
+        assertThat(t.getCause() instanceof UnknownHostException);
     }
 
     @Test
